96639a488f82aca8112796ecb95ed3284b08b78f
gift07-tester <giftmdlalose007@gmail.com>
2024-08-24 09:10:22 +0200

    Dog class and its object created

---
+++ src/test/java/com/serenitydojo/Cat.java
@@ -0,0 +1,26 @@
+package com.serenitydojo;
+
+public class Cat {
+    private String name;
+    private String favoriteFood;
+    private int age;
+
+
+    public Cat(String name, String favoriteFood, int age){
+        this.name = name;
+        this.favoriteFood = favoriteFood;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getFavoriteFood(){
+        return favoriteFood;
+    }
+    public int getAge(){
+        return age;
+    }
+
+
+}
--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -4,4 +4,41 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class WhenCreatingObjects {
+    @Test
+    public void creating_a_cat(){
+        String name = "felix";
+        String favoriteFood = "Tuna";
+        int age = 5;
+
+        Cat felix = new Cat("Felix","Tuna",5);
+        System.out.println(felix.getName());
+        System.out.println(felix.getFavoriteFood());
+        System.out.println(felix.getAge());
+
+        Cat spot = new Cat("Spot","Salmon",3);
+        System.out.println(spot.getName());
+        System.out.println(spot.getFavoriteFood());
+
+
+
+
+    }
+
+    @Test
+    public void creating_a_dog(){
+        String name = "Fido";
+        String favoriteToy = "Bone";
+        int age = 5;
+
+        Dog fido = new Dog("Fido","Bone",5);
+        Assert.assertEquals(fido.getName(),"Fido");
+        Assert.assertEquals(fido.getFavoriteToy(),"Bone");
+        Assert.assertEquals(fido.getAge(),5);
+
+
+
+
+    }
+
+
 }
